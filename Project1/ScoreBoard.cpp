b63deb8f115c05ad842fa60faa2999ef517c5943
ninomsu <masternino5x@gmail.com>
2017-10-17 12:12:32 -0400

    readded the first 0 in seconds timer

--- Project1/ScoreBoard.cpp
+++ Project1/ScoreBoard.cpp
@@ -37,19 +37,24 @@ CScoreBoard::~CScoreBoard()
 /// Draw the socreboard
 void CScoreBoard::OnDraw(Gdiplus::Graphics * graphics, double elapsed, bool gameover)
 {
-	wstringstream time;
 	if (!gameover)
 	{
 		mTotalTime += elapsed;
 	}
 	int seconds = (int)mTotalTime % 60;
 	int minutes = mTotalTime / 60;
-	time << minutes << ":" << seconds;
+	wstring secondsString = to_wstring(seconds);
+	if (seconds < 10) {
+		secondsString = to_wstring(0) + secondsString;
+	}
+	wstring fullTimeFormat = to_wstring(minutes) + L":" + secondsString;
+
+	const wchar_t* counter = fullTimeFormat.c_str();
 	FontFamily fontFamily(L"Arial");
 	Gdiplus::Font font(&fontFamily, 30);
 	SolidBrush green(Color(0, 255, 0));
 	
-	graphics->DrawString(time.str().c_str(),  // String to draw
+	graphics->DrawString(counter,  // String to draw
 		-1,         // String length, -1 means it figures it out on its own
 		&font,      // The font to use
 		PointF(534, -500),   // Where to draw (top left corner)
